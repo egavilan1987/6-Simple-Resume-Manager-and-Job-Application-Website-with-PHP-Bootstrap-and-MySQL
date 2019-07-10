5dd77929dc8cc19b34edc0d362d0e19a9171b45d
egavilan1987 <enmanuelgc@gmail.com>
2019-07-10 00:02:08 -0400

    today

--- db/egm_applications.sql
+++ db/egm_applications.sql
@@ -1,34 +1,31 @@
-CREATE DATABASE egm_applications;
+CREATE DATABASE egm_application;
 
-USE egm_applications;
+USE egm_application;
 
-CREATE TABLE tbl_users(
-	id_user INT NOT NULL AUTO_INCREMENT,
-	user_firstname VARCHAR(50) NOT NULL,
-	user_lastname VARCHAR(50) NOT NULL,
-	user_email VARCHAR(50) NOT NULL,
-	user_name VARCHAR(50) NOT NULL,
-	user_password text(50) NOT NULL,
-	created_date DATETIME NOT NULL,
-	PRIMARY KEY(id_user)
+CREATE TABLE tbl_admin(
+	admin_id INT(11) NOT NULL AUTO_INCREMENT,
+	admin_name VARCHAR(50) NOT NULL,
+	admin_password VARCHAR(150) NOT NULL,
+	PRIMARY KEY(admin_id)
 	);
-CREATE TABLE tbl_candidates(
-	id_candidate INT(11) NOT NULL AUTO_INCREMENT,
+
+CREATE TABLE tbl_candidate(
+	candidate_id INT(11) NOT NULL AUTO_INCREMENT,
 	candidate_firstname VARCHAR(50) NOT NULL,
 	candidate_lastname VARCHAR(50) NOT NULL,
 	candidate_email VARCHAR(50) NOT NULL,
 	candidate_phone VARCHAR(10) NOT NULL,
 	candidate_address VARCHAR(100) NOT NULL,
-             candidate_resume VARCHAR(50) NOT NULL,
+    candidate_resume VARCHAR(50) NOT NULL,
 	created_date DATETIME NOT NULL,
-	PRIMARY KEY(id_contact)
+	PRIMARY KEY(candidate_id)
 );
-CREATE TABLE tbl_positions(
-	id_position INT(11) NOT NULL AUTO_INCREMENT,
-	id_user INT NOT NULL,
+
+CREATE TABLE tbl_position(
+	position_id INT(11) NOT NULL AUTO_INCREMENT,
 	position_name VARCHAR(50) NOT NULL,
 	position_description VARCHAR(1000) NOT NULL,
 	position_requirement VARCHAR(1000) NOT NULL,	
-             created_date DATETIME NOT NULL,
-	PRIMARY KEY(id_position)
+    created_date DATETIME NOT NULL,
+	PRIMARY KEY(position_id)
 );
